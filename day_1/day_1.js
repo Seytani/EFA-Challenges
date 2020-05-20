5c24937e224dc5c7e606edeb276ccdc1b9f2f52f
Seytani <jessy_lee12@hotmail.com>
2020-05-19 21:49:54 -0400

    Challenges Tuesday 5/19 Complete

--- day_1/day_1.js
+++ day_1/day_1.js
@@ -1,3 +1,5 @@
+//Challenge 1
+
 let myName = 'Jess';
 let friendName = 'Nary';
 let difference;
@@ -13,3 +15,23 @@ if (myName.length == 0 || friendName.length == 0) {
     difference = myName.length - friendName.length;
     console.log(`My name is ${myName} and it has ${myName.length} letters. It is longer than my friend's name which is ${friendName}, by ${difference} letters.`);
 }
+
+//Challenge 2
+let obj = {str: 'Hello', num: 8, bool: true, obj2: {color: 'red', size: 3}, arr: [1,2,3], func: ()=>{return true}};
+console.log(typeof obj.func);
+
+if(typeof obj.func === 'string') {
+    console.log('you are okay');
+} else if (typeof obj.func === 'number') {
+    console.log('you are okay');
+} else if (typeof obj.func === 'boolean') {
+console.log('you are okay');
+} else if (typeof obj.func === 'object') {
+    console.log('you are okay');
+} else {
+    console.log('What are you?');}
+
+(typeof obj.func === 'string')  ? console.log('You are a String')  : 
+(typeof obj.func === 'number')  ? console.log('You are a Number')  : 
+(typeof obj.func === 'boolean') ? console.log('You are a Boolean') : 
+(typeof obj.func === 'object')  ? console.log('You are an Object') : console.log('What are you?');
